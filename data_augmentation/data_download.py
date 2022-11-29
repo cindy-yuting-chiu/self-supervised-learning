fd478e9ab6a7d3cfdc4a346f14e07eb358281cfc
Cindy Chiu <yc506@duke.edu>
2022-11-29 14:00:58 -0500

    change data aug to cifar 10 setting

--- data_augmentation/data_download.py
+++ data_augmentation/data_download.py
@@ -4,10 +4,10 @@ import torchvision
 import torchvision.transforms as transforms
 from aug_pair_generator import AugmentationPair
 
-def get_color_distortion(s:float=1.0):
+def get_color_distortion(s:float=0.5):
     """
     Function from the paper that create color distortion 
-    s: float, the strength of color distortion 
+    s: float, the strength of color distortion, for CIFAR 10, the paper use 0.5
     """
     color_jitter = transforms.ColorJitter(0.8*s, 0.8*s, 0.8*s, 0.2*s)
     rnd_color_jitter = transforms.RandomApply([color_jitter], p=0.8)
@@ -21,8 +21,8 @@ class ContrastLearningData:
         self.download_folder = download_folder
     def get_traindata(self):
         train_transform = transforms.Compose([
-            transforms.GaussianBlur(23, sigma=(0.1, 2.0)),
-            transforms.RandomResizedCrop(size=224,scale=(0.08,0.1),ratio=(0.75,1.33)),
+            #transforms.GaussianBlur(23, sigma=(0.1, 2.0)), # CIFAR 10 doesn't use gaussian blur
+            transforms.RandomResizedCrop(size=32,scale=(0.08,0.1),ratio=(0.75,1.33)),
             transforms.RandomHorizontalFlip(p=0.5),
             get_color_distortion(),
             transforms.ToTensor(),
